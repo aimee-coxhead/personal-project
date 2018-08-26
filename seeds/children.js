1d1d766c6cd70c83c8e19a33e2db2f2fe1d17aea
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-08-26 21:47:12 +1200

    working on CSS

--- seeds/children.js
+++ seeds/children.js
@@ -4,7 +4,7 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('children').insert([
-        {id: 88801, name: 'Bob', profile_photo: '../images/dog.jpg', timetable_Id: 99901},
+        {id: 88801, name: 'Bob', profile_photo: 'images/dog.jpg', timetable_Id: 99901},
         {id: 88802, name: 'Grace', profile_photo: '1_1', timetable_Id: 99902},
         {id: 88803, name: 'Sally', profile_photo: '+_+', timetable_Id: 99903}
       ])
--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const homeRoutes = require('./routes')
 const server = express()
 
 server.engine('hbs', hbs({
+  defaultLayout: 'main',
   extname: 'hbs'
 }))
+
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
 
 server.use('/', homeRoutes)
 
